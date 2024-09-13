1b07e89728b55346d3ff26d5516e2f9aa30d727b
AJ <14wright08@gmail.com>
2024-09-13 11:40:20 +0100

    Gripper and camera

--- moveit_gripper/src/moveit_gripper.cpp
+++ moveit_gripper/src/moveit_gripper.cpp
@@ -18,7 +18,6 @@ int main(int argc, char* argv[])
   request->position = 0; 
   request->speed = 255;
   request->force = 255;
-
   // Call the gripper service asynchronously
   auto future = gripper_client->async_send_request(request);
 
@@ -34,7 +33,9 @@ int main(int argc, char* argv[])
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, "ur_with_gripper");
-
+  for (const auto& str : move_group_interface.getCurrentJointValues()) {
+    RCLCPP_INFO(logger, "%2f",str);
+  }
   // Set a target Pose
 
 auto const collision_object = [frame_id = move_group_interface.getPlanningFrame(), &node, &logger] {
@@ -162,6 +163,196 @@ auto const vial = [frame_id = move_group_interface.getPlanningFrame(), &node, &l
     return vial;
   }();
   planning_scene_interface.applyCollisionObject(vial);
+
+  auto const ceiling = [frame_id = move_group_interface.getPlanningFrame(), &node, &logger] {
+   
+    // Print the planning frame for debugging purposes
+    RCLCPP_INFO(logger, "Planning frame: %s", frame_id.c_str());
+ 
+    // Initialize a CollisionObject message
+    moveit_msgs::msg::CollisionObject ceiling;
+   
+    // Set the frame ID for the collision object
+    ceiling.header.frame_id = frame_id;
+   
+    // Set the timestamp to the current time
+    ceiling.header.stamp = node->now();
+   
+    // Assign a unique ID to the collision object
+    ceiling.id = "ceiling";
+ 
+    // Define the shape of the collision object as a box
+    shape_msgs::msg::SolidPrimitive primitivev;
+    primitivev.type = primitivev.BOX;
+    primitivev.dimensions.resize(3);
+   
+    // Set the dimensions of the box (in meters)
+    primitivev.dimensions[primitivev.BOX_X] = 5.0;  // Width
+    primitivev.dimensions[primitivev.BOX_Y] = 0.0125;  // Depth
+    primitivev.dimensions[primitivev.BOX_Z] = 5.0;  // Height
+ 
+    // Define the pose (position and orientation) of the box
+    geometry_msgs::msg::Pose box_pose2;
+    // Set the position of the box center
+    box_pose2.position.x = 0.0; // meters in x-direction
+    box_pose2.position.y = 0.4;   // Centered in y-direction
+    box_pose2.position.z = 0.7; // meters in z-direction
+   
+    // Set the orientation of the box (no rotation in this case)
+    box_pose2.orientation.x = 0.47;
+    box_pose2.orientation.y = 0.52;
+    box_pose2.orientation.z = 0.49;
+    box_pose2.orientation.w = 0.51;
+ 
+    // Add the shape and pose to the collision object
+    ceiling.primitives.push_back(primitivev);
+    ceiling.primitive_poses.push_back(box_pose2);
+   
+    // Set the operation to add the object to the planning scene
+    ceiling.operation = ceiling.ADD;
+ 
+    // Log information about the created collision object
+    RCLCPP_INFO(logger, "Created collision object: %s", ceiling.id.c_str());
+     
+    RCLCPP_INFO(logger, "Box dimensions: %.2f x %.2f x %.2f", 
+      primitivev.dimensions[primitivev.BOX_X],
+      primitivev.dimensions[primitivev.BOX_Y],
+      primitivev.dimensions[primitivev.BOX_Z]);
+       
+    RCLCPP_INFO(logger, "Box position: (%.2f, %.2f, %.2f)",
+      box_pose2.position.x, box_pose2.position.y, box_pose2.position.z);
+ 
+    // Return the fully defined collision object
+    return ceiling;
+  }();
+  planning_scene_interface.applyCollisionObject(ceiling);
+  auto const wall1 = [frame_id = move_group_interface.getPlanningFrame(), &node, &logger] {
+   
+    // Print the planning frame for debugging purposes
+    RCLCPP_INFO(logger, "Planning frame: %s", frame_id.c_str());
+ 
+    // Initialize a CollisionObject message
+    moveit_msgs::msg::CollisionObject wall1;
+   
+    // Set the frame ID for the collision object
+    wall1.header.frame_id = frame_id;
+   
+    // Set the timestamp to the current time
+    wall1.header.stamp = node->now();
+   
+    // Assign a unique ID to the collision object
+    wall1.id = "wall1";
+ 
+    // Define the shape of the collision object as a box
+    shape_msgs::msg::SolidPrimitive primitivev;
+    primitivev.type = primitivev.BOX;
+    primitivev.dimensions.resize(3);
+   
+    // Set the dimensions of the box (in meters)
+    primitivev.dimensions[primitivev.BOX_X] = 5.0;  // Width
+    primitivev.dimensions[primitivev.BOX_Y] = 0.0125;  // Depth
+    primitivev.dimensions[primitivev.BOX_Z] = 5.0;  // Height
+ 
+    // Define the pose (position and orientation) of the box
+    geometry_msgs::msg::Pose box_pose2;
+    // Set the position of the box center
+    box_pose2.position.x = 0.0; // meters in x-direction
+    box_pose2.position.y = -0.3;   // Centered in y-direction
+    box_pose2.position.z = 0.8; // meters in z-direction
+   
+    // Set the orientation of the box (no rotation in this case)
+    box_pose2.orientation.x = 0.0;
+    box_pose2.orientation.y = 0.0;
+    box_pose2.orientation.z = 1.0;
+    box_pose2.orientation.w = 0.0;
+ 
+    // Add the shape and pose to the collision object
+    wall1.primitives.push_back(primitivev);
+    wall1.primitive_poses.push_back(box_pose2);
+   
+    // Set the operation to add the object to the planning scene
+    wall1.operation = wall1.ADD;
+ 
+    // Log information about the created collision object
+    RCLCPP_INFO(logger, "Created collision object: %s", wall1.id.c_str());
+     
+    RCLCPP_INFO(logger, "Box dimensions: %.2f x %.2f x %.2f", 
+      primitivev.dimensions[primitivev.BOX_X],
+      primitivev.dimensions[primitivev.BOX_Y],
+      primitivev.dimensions[primitivev.BOX_Z]);
+       
+    RCLCPP_INFO(logger, "Box position: (%.2f, %.2f, %.2f)",
+      box_pose2.position.x, box_pose2.position.y, box_pose2.position.z);
+ 
+    // Return the fully defined collision object
+    return wall1;
+  }();
+  planning_scene_interface.applyCollisionObject(wall1);
+
+  auto const wall2 = [frame_id = move_group_interface.getPlanningFrame(), &node, &logger] {
+   
+    // Print the planning frame for debugging purposes
+    RCLCPP_INFO(logger, "Planning frame: %s", frame_id.c_str());
+ 
+    // Initialize a CollisionObject message
+    moveit_msgs::msg::CollisionObject wall2;
+   
+    // Set the frame ID for the collision object
+    wall2.header.frame_id = frame_id;
+   
+    // Set the timestamp to the current time
+    wall2.header.stamp = node->now();
+   
+    // Assign a unique ID to the collision object
+    wall2.id = "wall2";
+ 
+    // Define the shape of the collision object as a box
+    shape_msgs::msg::SolidPrimitive primitivev;
+    primitivev.type = primitivev.BOX;
+    primitivev.dimensions.resize(3);
+   
+    // Set the dimensions of the box (in meters)
+    primitivev.dimensions[primitivev.BOX_X] = 5.0;  // Width
+    primitivev.dimensions[primitivev.BOX_Y] = 0.0125;  // Depth
+    primitivev.dimensions[primitivev.BOX_Z] = 5.0;  // Height
+ 
+    // Define the pose (position and orientation) of the box
+    geometry_msgs::msg::Pose box_pose2;
+    // Set the position of the box center
+    box_pose2.position.x = 0.0; // meters in x-direction
+    box_pose2.position.y = 0.5;   // Centered in y-direction
+    box_pose2.position.z = 0.8; // meters in z-direction
+   
+    // Set the orientation of the box (no rotation in this case)
+    box_pose2.orientation.x = 0.0;
+    box_pose2.orientation.y = 1.0;
+    box_pose2.orientation.z = 0.0;
+    box_pose2.orientation.w = 0.0;
+ 
+    // Add the shape and pose to the collision object
+    wall2.primitives.push_back(primitivev);
+    wall2.primitive_poses.push_back(box_pose2);
+   
+    // Set the operation to add the object to the planning scene
+    wall2.operation = wall2.ADD;
+ 
+    // Log information about the created collision object
+    RCLCPP_INFO(logger, "Created collision object: %s", wall2.id.c_str());
+     
+    RCLCPP_INFO(logger, "Box dimensions: %.2f x %.2f x %.2f", 
+      primitivev.dimensions[primitivev.BOX_X],
+      primitivev.dimensions[primitivev.BOX_Y],
+      primitivev.dimensions[primitivev.BOX_Z]);
+       
+    RCLCPP_INFO(logger, "Box position: (%.2f, %.2f, %.2f)",
+      box_pose2.position.x, box_pose2.position.y, box_pose2.position.z);
+ 
+    // Return the fully defined collision object
+    return wall2;
+  }();
+  planning_scene_interface.applyCollisionObject(wall2);
+
+
   // Create a plan to that target pose
   auto const target_pose = [] {
     geometry_msgs::msg::Pose msg;
@@ -179,10 +370,7 @@ auto const vial = [frame_id = move_group_interface.getPlanningFrame(), &node, &l
     msg.position.x =-0.125456;
     msg.position.y = 0.379510;
     msg.position.z = 0.282653;
-    msg.orientation.w = 0.014040;
-    msg.orientation.x = 0.998404;
-    msg.orientation.y = 0.052713;
-    msg.orientation.z = 0.014626;
+
     return msg;
   }();
   moveit_msgs::msg::PositionConstraint box_constraint;
@@ -202,9 +390,11 @@ auto const vial = [frame_id = move_group_interface.getPlanningFrame(), &node, &l
   box_constraint.weight = 1.0;
   moveit_msgs::msg::Constraints box_constraints;
   box_constraints.position_constraints.emplace_back(box_constraint);
-  move_group_interface.setPathConstraints(box_constraints);
+  //move_group_interface.setPathConstraints(box_constraints);
+  move_group_interface.setPlanningTime(60.0);
+  move_group_interface.setNumPlanningAttempts(1000);
   move_group_interface.setPoseTarget(target_pose);
-  move_group_interface.setPlanningTime(30.0);
+  
   auto const [success, plan] = [&move_group_interface] {
     moveit::planning_interface::MoveGroupInterface::Plan msg;
     auto const ok = static_cast<bool>(move_group_interface.plan(msg));
@@ -250,12 +440,14 @@ auto const vial = [frame_id = move_group_interface.getPlanningFrame(), &node, &l
     msg2.position.x =-0.433011;
     msg2.position.y = 0.013971;
     msg2.position.z = 0.203771;
-    msg2.orientation.w = -0.034168;
-    msg2.orientation.x = 0.780949;
-    msg2.orientation.y = 0.623642;
-    msg2.orientation.z = -0.004659;
+    msg2.orientation.w = 0.013801;
+    msg2.orientation.x = 0.998445;
+    msg2.orientation.y = 0.051797;
+    msg2.orientation.z = 0.015303;
     return msg2;
   }();
+  move_group_interface.setPlanningTime(60.0);
+  move_group_interface.setNumPlanningAttempts(1000);
   move_group_interface.setPoseTarget(target_place);
   auto const [success2, plan2] = [&move_group_interface] {
     moveit::planning_interface::MoveGroupInterface::Plan msg2;
